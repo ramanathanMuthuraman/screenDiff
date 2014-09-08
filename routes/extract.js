23d39a92584afb6fb94e64e9bcdc84f9d7c87ff9
ramanathan <ramanathan.gksvv@gmail.com>
2014-09-08 07:58:28 +0000

    file traversal

--- routes/extract.js
+++ routes/extract.js
@@ -1,8 +1,7 @@
 var express = require('express');
 var fs = require('fs');
 var unzip = require('unzip');
-var path = require('path');
-var recursive = require('recursive-readdir');
+var walk = require('walk');
 var fast_image_size = require('fast-image-size');
 var router = express.Router();
 
@@ -16,41 +15,39 @@ router.post('/', function(req, res) {
     fs.rename(req.files.decompress.path, outputPath + zipFolder, function(err) {
         if (err)
             return;
+        //unzip the folder
         fs.createReadStream(outputPath + zipFolder).pipe(unzip.Extract({
             path: outputPath + unzippedFolder
         }).on('close', function() {
-            recursive(outputPath + unzippedFolder, function(err, files) {
-                if (err) throw err;
-                var folder = unzippedFolder;
-                var fileName = [];
-                var totalNumberOfImages=files.length;
-                for (file in files) {
-                    
-                    fast_image_size(files[file], function(ret_obj) {
-                        fileName.push(path.basename(ret_obj.image));
-                        console.log('Testing: ' + ret_obj.image);
-                        console.log('Type: ' + ret_obj.type);
-                        console.log('Image width: ' + ret_obj.width);
-                        console.log('Image height: ' + ret_obj.height);
-                        
-                        console.log(file,totalNumberOfImages-1)
-                    if(file === totalNumberOfImages-1){
-                           console.log(file,totalNumberOfImages-1)
-                      
-                        res.send({
-                            path: ""
-                        });
+            //when unzip is complete send the image info to the user
+            var path ={};
+            //get the folder name 
+            path.root = unzippedFolder;
+            var images = [];
 
-                    }
-                        
-                    });
-                   
-                }
-
+            walker = walk.walk(outputPath + unzippedFolder);
+           //traverse the files
+            walker.on("file", function(root, fileStats, next) {
+                 var image = {};
                
-            });
-            
+                fast_image_size(root + '/' + fileStats.name, function(ret_obj) {
+                   //get the image name, width and height 
+                    image.name = fileStats.name;
+                    image.width = ret_obj.width;
+                    image.height = ret_obj.height;
+                    images.push(image);
+                    next();
+                });
 
+            });
+
+            walker.on('end', function() {
+                path.images = images;
+                //after the file traversal is over send the info to UI
+                res.send({
+                    "path": path
+                });
+            });
 
 
         }));

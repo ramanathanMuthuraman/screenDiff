268610c2a44bd5b929582b45f611b7c3724277b0
ramanathan <ramanathan.gksvv@gmail.com>
2014-09-15 11:00:11 +0000

    file uplaod

--- public/js/app.js
+++ public/js/app.js
@@ -1,37 +1,8 @@
-function validateName(el) {
-    var name = el.val();
-    var retValue = {};
 
-    if (name == "") {
-        retValue.status = false;
-        retValue.msg = "Please enter a name";
-    }
-    else {
-        retValue.status = true;
-    }
-
-    return retValue;
-}
 
 $(document).ready(function() {
 
  
-    var appConfig = {
-        "height": $(window).height() - 150,
-        "width": $(window).width() - 150
-    }
-    $.fn.wizard.logging = true;
-    var wizard = $('#some-wizard').wizard({
-        keyboard: false,
-        contentHeight: appConfig.height,
-        contentWidth: appConfig.width,
-        backdrop: false,
-        submitUrl: "/extract",
-        showClose: false,
-        container: "#main" // new config to append content inside the DOM specified
-    });
-
-    wizard.show();
     // Check to see when a user has selected a file                                                                                                                
 
    /* $("#archiveFolder").click(function() {
